6df4bd81a6c94ac2783b5ca28d25152cf244456e
Ping-Ju, Chen <cpr860632231@gmail.com>
2023-03-04 18:57:25 -0500

    debug

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -12,7 +12,7 @@ const Landing = ({ setSelectedPage }) => {
       className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
     >
       {/*IMAGE SECTION */}
-      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+      <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
         {isAboveMediumScreens ? (
           <div
             className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
